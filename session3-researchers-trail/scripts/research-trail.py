2b86a63f2c1780a6e58409f10bf81c4cf5938489
Sara Alwadia <sara.wadia2016@gmail.com>
2026-06-18 12:45:16 +0300

    CP2 complete

--- session3-researchers-trail/scripts/research-trail.py
+++ session3-researchers-trail/scripts/research-trail.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 df = pd.read_csv("../raw_data/researchers.csv")
 
@@ -14,3 +15,17 @@ letters = filtered["last_name"].str[0]
 word = "".join(letters)
 
 print("CP1 RESULT:", word) # CP1 RESULT: DATAOPENSDOORS
+
+#############################################################
+
+
+with open("../raw_data/publications.json", "r") as f:
+    data = json.load(f)
+
+df = pd.json_normalize(data)
+print(df.head())
+
+top_paper = df.sort_values("citations", ascending=False).iloc[0]
+
+print("Top Paper Title:", top_paper["title"]) # Top Paper Title: Data Opens Doors: A Manifesto for Open Research
+print("Researcher ID:", top_paper["researcher_id"]) # Researcher ID: R001
